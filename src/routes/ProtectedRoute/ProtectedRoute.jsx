91c7f7d3d8a6e899b5ec4fdbc6e7e5fe7854cdd1
Olim Yuildoshev <oyuldoshev39@gmail.com>
2024-04-02 13:44:11 +0500

    Fixed a problem

--- src/routes/ProtectedRoute/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
   const isAuth = useSelector(({ auth }) => auth.isAuth);
 
-  return isAuth ? children : <Navigate to="/login" />;
+  return isAuth ? children : <Navigate to="/signin" />;
 };
 
 export default ProtectedRoute;
